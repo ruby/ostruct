388869cc5d816362fb95d954e9897fbc5e7b4d79
Abhilash Reddy <abhilashmv@outlook.com>
2018-10-16 13:48:12 +0530

    Passing options as an argument to initializer instead of is_recursive,

--- lib/ostruct.rb
+++ lib/ostruct.rb
@@ -87,13 +87,14 @@ class OpenStruct
   #
   #   data   # => #<OpenStruct country="Australia", capital="Canberra">
   #
-  def initialize(hash=nil, is_recursive=false)
+  def initialize(hash=nil, options={recursive: true})
     @table = {}
-    @recursive = is_recursive
+    @recursive = options.fetch(:recursive, false)
+    puts @recursive
     if hash
       hash.each_pair do |k, v|
         k = k.to_sym
-        @table[k] = (@recursive && v.respond_to?(:to_hash)) ? OpenStruct.new(v,true) : v
+        @table[k] = (@recursive && v.respond_to?(:to_hash)) ? OpenStruct.new(v, {recursive: true}) : v
       end
     end
   end
@@ -202,7 +203,7 @@ class OpenStruct
       if len != 1
         raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
       end
-      modifiable?[new_ostruct_member!(mname)] = (@recursive && args[0].respond_to?(:to_hash)) ? OpenStruct.new(args[0],true) : args[0]
+      modifiable?[new_ostruct_member!(mname)] = (@recursive && args[0].respond_to?(:to_hash)) ? OpenStruct.new(args[0], {recursive: true}) : args[0]
     elsif len == 0 # and /\A[a-z_]\w*\z/ =~ mid #
       if @table.key?(mid)
         new_ostruct_member!(mid) unless frozen?
