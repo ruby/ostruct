03b3e65a564bdfe3f78c44342218cd8b32aec04d
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-04-28 17:57:27 +0900

    use assert_ractor provided by CoreAssertions

--- test/ostruct/test_ostruct.rb
+++ test/ostruct/test_ostruct.rb
@@ -343,14 +343,16 @@ class TC_OpenStruct < Test::Unit::TestCase
   end
 
   def test_ractor
-    obj1 = OpenStruct.new(a: 42, b: 42)
-    obj1.c = 42
-    obj1.freeze
+    assert_ractor(<<~RUBY, require: 'ostruct')
+      obj1 = OpenStruct.new(a: 42, b: 42)
+      obj1.c = 42
+      obj1.freeze
 
-    obj2 = Ractor.new obj1 do |obj|
-      obj
-    end.take
-    assert obj1.object_id == obj2.object_id
+      obj2 = Ractor.new obj1 do |obj|
+        obj
+      end.take
+      assert obj1.object_id == obj2.object_id
+    RUBY
   end if defined?(Ractor)
 
   def test_legacy_yaml
